9c2e838b8b709800bfd6a2d838f15f3bafc2f01f
Asep <asepjourney551@gmail.com>
2026-09-18 13:27:18 +0700

    update proyek

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class JastipApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
     return MaterialApp.router(
-      title: 'Jastip Wirosari',
+      title: 'Jastip Bro',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       routerConfig: router,
